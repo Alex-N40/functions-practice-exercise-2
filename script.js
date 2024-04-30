1a0f0428d29afd8f18e1110b016f8a63ce308a44
Alex-N40 <alex_n40@proton.me>
2024-04-30 10:29:54 -0500

    Added console results as comments

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ var multiply = function (num1, num2) {
 };
 
 console.log(multiply(2, 2));
+// 4
 console.log(multiply("abc", 4));
+// Please provide two numbers.
 console.log(multiply(2, "abc"));
+// Please provide two numbers.
 console.log(multiply("abc", "abc"));
+// Please provide two numbers.
 console.log(multiply(2.5, 2));
+// 5
 console.log(multiply(2, 3.5));
+// 7
